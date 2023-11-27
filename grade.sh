ad6b306636ff397da00df0be611e51f7a38b2fa0
NaruKoba <101598791+NaruKoba@users.noreply.github.com>
2023-11-27 14:43:30 -0800

    fixed grade.sh

--- grade.sh
+++ grade.sh
@@ -8,6 +8,22 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+set -e
+
+tests= `find student-submission -name "*.java"`
+
+for file in $tests
+do
+    if[[-f $file ]]
+        then
+            echo "file, $file, exits"
+    else if 
+done
+
+cp -r ./student-submission ./grading-area
+cp -r lib ./grading-area
+
+javac -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" ./grading-area/*java
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
